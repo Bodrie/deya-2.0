cab13cef54e027aff5efb819651b7de41e6fa79d
Dobromir Kirov <kirov0407@gmail.com>
2022-11-09 10:48:00 +0200

    ne sym programistka

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,3 +2,13 @@ declare module "*.png";
 declare module "*.svg";
 declare module "*.jpeg";
 declare module "*.jpg";
+
+interface Window {
+    chrome: Chrome;
+  }
+  
+  interface Chrome {
+    webview: WebView2;
+  }
+  
+  interface WebView2 {}
