c01b8963fc9b56a68760b34e65a5afff4b9cbe96
Dobromir Kirov <80153626+Bodrie@users.noreply.github.com>
2022-11-10 09:23:00 +0200

    Auth extending (#3)
    * started to extend the auth, in need of reliable user managment
    
    * added emial verification, extended ways of user manegment, other small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useAuth, useEmialVerification } from "./hooks";
 import { Routes, Route } from "react-router-dom";
 import { Header, Footer } from "./components";
@@ -6,32 +6,8 @@ import { Home, Calendar, Admin, Auth, UserProfile } from "./views";
 import "./styles/App.css";
 
 function App() {
-  const test = () => {
-    // @ts-ignore
-    let standalone = window.navigator.standalone;
-    
-    let userAgent = window.navigator.userAgent.toLowerCase();
-    let safari = /safari/.test(userAgent);
-    let ios = /iphone|ipod|ipad/.test(userAgent);
-
-    if (ios) {
-      if (!standalone && safari) {
-        document.getElementById("where-am-i")!.textContent = "browser";
-      } else if (standalone && !safari) {
-        document.getElementById("where-am-i")!.textContent = "standalone";
-      } else if (!standalone && !safari) {
-        document.getElementById("where-am-i")!.textContent = "uiwebview";
-      }
-    } else {
-      document.getElementById("where-am-i")!.textContent = "not iOS";
-    }
-  };
-
-  useEffect(() => {
-    test();
-  }, []);
   const userData = useAuth();
-  useEmialVerification(userData);
+  useEmialVerification(userData)
 
   return (
     <div className="App">
